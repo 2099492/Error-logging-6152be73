c225c02b71625d4d092f9857f1951b027bb802ff
2099492 <54841481+2099492@users.noreply.github.com>
2019-12-09 16:52:26 +0100

    Add files via upload

--- error_log.php
+++ error_log.php
@@ -1,7 +1,6 @@
 <?php
 $a = 2;
 if ($a != 1) {
-  error_log("ERROR $a is not 1", 0, "/xampp/htdocs/website/PHP - web/Error-logging-6152be73/errors.log");
-  echo "ERROR $a is not 1";
+  error_log("ERROR $a is not 1", 3, "/xampp/htdocs/website/PHP - web/Error-logging-6152be73/errors.log");
 }
 ?>
